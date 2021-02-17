9e4640f9589519e15bc63af2fab2d7a2003db913
cparros <celparros@gmail.com>
2021-02-17 16:26:59 -0500

    added references for all cli application results

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+
+
 inquirer.prompt([
   {
     type: 'confirm',
@@ -38,6 +40,9 @@ inquirer.prompt([
     name: 'addition'
   },
 ]).then(response => {
+  let newManager = response.name
+  newManager = new Manager(response.name, response.email, response.id, response.officeNumber)
+
   console.log('Here are your Manager results:')
   console.log(response)
   inquirer.prompt([
@@ -48,8 +53,10 @@ inquirer.prompt([
     choices: ['Engineer', 'Intern']
   },
 ]).then(response => {
-  console.log(`Here are your Manager\'s results:`)
+
+  console.log(`Here are your next employee\'s results:`)
   console.log(response)
+
   if(response.employee[0] === 'Engineer'){
     inquirer.prompt([
       {
@@ -73,6 +80,8 @@ inquirer.prompt([
         name: 'github'
       }
     ]).then(response => {
+      let newEngineer = response.name
+      newEngineer = new Engineer(response.name,  response.id, response.email, response.github )
       console.log(`Here are your Engineer\'s results:`)
       console.log(response)
     })
@@ -99,8 +108,29 @@ inquirer.prompt([
     name: 'school'
   }
     ]).then(response => {
+    let newIntern = response.name
+    newIntern = new Intern(response.name, response.id, response.email, response.school)
     console.log(`Here are your Intern\'s results:` + response)
     })
   })
 
+}).then(response => {
+  
+  const html = 
+  `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Your New Team</title></title>
+  </head>
+  <body style="background-color: darkgrey;">
+    <header class="jumbotron bg-dark" style="text-align: center; font-size: 34px; color: antiquewhite; font-weight: bolder; border-bottom: antiquewhite solid 5px;">Your New Team</header>
+  </body>
+  </html>
+  `
+
+  fs.writeFile('team.html', html, (err) => 
+err ? console.error(err) : console.log('Success!'))
 })
